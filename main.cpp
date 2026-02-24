2cacc1e0a6b13fa7588e67805be37ff474067040
Jay <jayrathore88155@gmail.com>
2026-02-24 19:19:07 +0530

    Draw a circle on the surface in the window b

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<SDL2/SDL.h>
+#include<cmath>
 
 #define WIDTH 1200
 #define HEIGHT 400
@@ -16,29 +17,52 @@ typedef struct Ray{
   double angle ;
 }Ray;
 
+void FillCircle(SDL_Surface* surface , Circle circle , Uint32 color){
+
+  double radius_squared = pow(circle.radius , 2);
+
+  /**
+   * We try to make a  Circle inside the rectangle 
+   * where the rectangle start from the (x - r) and goes to (x + r) in x-axis and in y axis it start from the (y - r) and goes to (y + r) 
+   * and then generate the pixel of the width and height 1 X 1 to fill the whole area 
+   */
+
+  for(double x = circle.x - circle.radius ; x <= circle.x + circle.radius ; x++){
+    for(double y = circle.y - circle.radius ; y <= circle.y + circle.radius ; y++){ 
+      double distance_squared = pow(x - circle.x , 2) + pow(y - circle.y  , 2);
+      
+      if(distance_squared < radius_squared){
+        SDL_Rect pixel = (SDL_Rect) {int(x), int(y), 1 , 1};
+        SDL_FillRect(surface , &pixel  , color);
+      }
+    }
+  }
+
+}
+
 int main(int argc  , char** args){
   SDL_Window* window = SDL_CreateWindow("Ray Tracing", SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , WIDTH , HEIGHT, 0);
 
   SDL_Surface* surface = SDL_GetWindowSurface(window);
-
-  SDL_Rect dest = (SDL_Rect) {10 , 10 , 10, 10};
-
-  SDL_FillRect(surface , &dest , COLOR_WHITE);
-
-  SDL_UpdateWindowSurface(window);
-
+  
+  Circle circle = {100 , 100 , 80};
+  
   SDL_Event event ;
   int running = 1;
-
+  
   while(running){
     while(SDL_PollEvent(&event)){
       if(event.type == SDL_QUIT){
         running = 0;
         SDL_Delay(10);
       }
+      
+      FillCircle(surface  , circle , COLOR_WHITE);
+      SDL_UpdateWindowSurface(window);
+      
     }
   }
-
+  
   SDL_DestroyWindow(window);
   SDL_Quit();
 
